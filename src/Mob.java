33d7a304209aeae2d0911dedb1346dfc9ca1e8d2
SIGAAMU <111403004+SIGAAMU@users.noreply.github.com>
2022-08-18 17:39:07 -0500

    Update Mob.java

--- src/Mob.java
+++ src/Mob.java
@@ -8,24 +8,51 @@ public class Mob {
     static int mob_wpn[] = {
 
     };
-    // the mobMove array is the possibilities the specific mob has of moving every game loop
+    static int mob_dmg[] = {
+    
+    };
+    static int mob_rng[] = {
+    
+    };
+    // the maximum possibilty a mob has of moving
     static float mobMove_max[] = {
 
     };
+    // the minimum possibility a mob has of moving
     static float mobMove_min[] = {
 
     };
+    // the required number to be rolled for a mob to move
     static float mobMove_required[] = {
 
     };
-    static float T_mobY = 0.0f;
-    static float T_mobX = 0.0f;
-    public String T_mobName;
-    public String T_mobWpn;
-    public int T_mobWpn_dmg;
+    // if monster moves, this is their speed
+    static float mob_speed[] = {
+    
+    };
+    static static float T_mobY = 0.0f;
+    static static float T_mobX = 0.0f;
+    public static String T_mobName;
+    public static String T_mobWpn;
+    public static int T_mobWpn_rng;
+    public static int T_mobWpn_dmg;
+    // this series of functions only allows 1 mob at a time, but meh
+    public void Mob(String mobName)
+    {
+        T_mobName = mobName;
+        getMob_stat();
+    }
     public void getMob_stat()
     {
-
+        for (int i = 0; i < mob_ls.length; i++)
+        {
+            if (mob_ls[i] == T_mobName)
+            {
+                T_mobWpn = mob_wpn[i];
+                T_mobWpn_dmg = mob_dmg[i];
+                T_mobWpn_rng = mob_rng[i];
+            }
+        }
     }
     public void moveMob()
     {
@@ -33,7 +60,27 @@ public class Mob {
         {
             if (mob_ls[i] == T_mobName)
             {
-                T_mobY = (float)(Math.random()*(mobMove_max[i]-mobMove_min[i]+1)+mobMove_min[i]);
+                float moveBool = (float)(Math.random()*(mobMove_max[i]-mobMove_min[i]+1)+mobMove_min[i]);
+                if (moveBool >= mobMove_required[i])
+                {
+                    int randMove = (int)(Math.random()*(4-1+1)+1);
+                    if (randMove == 1) // north/w
+                    {
+                        T_mobY = - (1.0f * mob_speed[i]);
+                    }
+                    else if (randMove == 2) // west/a
+                    {
+                        T_mobX = - (1.0f * mob_speed[i]);
+                    }
+                    else if (randMove == 3) // south/s
+                    {
+                        T_mobY = + (1.0f * mob_speed[i]);
+                    }
+                    else if (randMove == 4) // east/d
+                    {
+                        T_mobX = + (1.0f * mob_speed[i]);
+                    }
+                }
             }
         }
     }
