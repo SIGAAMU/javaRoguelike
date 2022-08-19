40d2808cf3a15b16214acd19c34f0c8b93d5ea80
Blitz012 <111456534+Blitz012@users.noreply.github.com>
2022-08-18 20:59:18 -0500

    Update Item.java

--- src/Item.java
+++ src/Item.java
@@ -4,6 +4,31 @@ public class Item
         "Sword",
         "Mace",
         "Bow",
+        "Bulldog",
+        "Voldemort's Stick(the ultimate poker)",
+        "Lance",
+        "Battle Axe",
+        "CrossBow",
+        "Dagger",
+        "Log",
+        "PitchFork",
+        
+        
+    };
+    
+    public int wpn_dmg[] = {
+        15,
+        25,
+        17,
+        30,
+        18,
+        20,
+        30,
+        24,
+        10,
+        35,
+        30,
+        
     };
     
     public void pickupWpn()
