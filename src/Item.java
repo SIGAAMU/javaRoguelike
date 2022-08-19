a50c7236278da9bb0bc78ecab70c2776bfb4918d
SIGAAMU <111403004+SIGAAMU@users.noreply.github.com>
2022-08-18 19:44:43 -0500

    Create Item.java

---
+++ src/Item.java
@@ -0,0 +1,12 @@
+public class Item
+{
+    public String wpn_ls[] = {
+        "Sword",
+    };
+    
+    public void pickupWpn()
+    {
+        Character playr = new Character();
+        if ()
+    };
+};
